54af66085c46ba269d2dcdf278f0a81103e64649
yufeng tian <yufengtian@newbanker.cn>
2021-07-15 09:17:16 +0800

    添加 css 模块化案例

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Button } from 'antd-mobile'
-import '@/app.scss'
-
+import css from './app.module.scss'
+import './app.scss'
 function App() {
   return (
     <div className="App">
       {
-        Array(50).fill(1).map((item, idx) => (<p className="p" key={idx}>一段自定义的css样式文字,第{idx}条</p>))
+        Array(50).fill(1).map((item, idx) => (<p className={css.p + ' p-fs'} key={idx}>一段自定义的css样式文字,第{idx}条</p>))
       }
       <Button type="primary">按钮</Button>
     </div>
